49e74a223442d9e03350d728da688dff45d785f9
Josh Meier <joshua.meier2@gmail.com>
2018-08-17 01:45:04 -0700

    Worked on main for a bit

--- inc/sh.h
+++ inc/sh.h
@@ -6,7 +6,7 @@
 /*   By: josh <jmeier@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/10 17:00:37 by josh              #+#    #+#             */
-/*   Updated: 2018/08/10 17:05:42 by josh             ###   ########.fr       */
+/*   Updated: 2018/08/17 01:43:36 by josh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,22 @@
 # include <sys/stat.h>
 # include <sys/wait.h>
 
+# define RESET "\033[0m"
+# define BOLD "\033[1m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+
 typedef struct	s_sh
 {
 	char		*line;
 	char		**env;
-	char		**path;
+	char		*exe;
+	char		**exe_a;
 }				t_sh;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,34 @@
 /*   By: josh <jmeier@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/10 16:47:59 by josh              #+#    #+#             */
-/*   Updated: 2018/08/10 16:51:43 by josh             ###   ########.fr       */
+/*   Updated: 2018/08/17 01:43:38 by josh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sh.h>
 
-int		main(int ac, char **av)
+void	ft_error(char *str)
 {
+	ft_printf(RED"%s"RESET, str);
+	write(1, "\n", 1);
+	exit(1);
+}
+
+
+
+int		main(int ac, char **av, char **envp)
+{
+	t_sh	sh;
+
 	(void)ac;
+	(void)av;
+	ft_bzero(&sh, sizeof(t_sh));
+	if (!(sh.env = ft_arrdup(envp)))
+		ft_error("Failed to copy environment variables.");
+	while (1)
+	{
+		read
+		ft_printf(PURPLE"%s "BLUE"jo.sh"GREEN BOLD" $>"RESET, cwd);
+	}
+	return (0);
 }
